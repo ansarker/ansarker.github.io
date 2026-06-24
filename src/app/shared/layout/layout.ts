95de576322bb541f1b36056ea886a28e0c8d9359
anis <anissarker603@gmail.com>
2026-06-24 23:58:03 +0600

    [Fix] Added layout + fixes the css for full-width for small window

--- src/app/shared/layout/layout.ts
+++ src/app/shared/layout/layout.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Social } from '../../components/social/social';
 import { Publications } from '../../components/publications/publications';
 import { Education } from '../../components/education/education';
 import { About } from '../../components/about/about';
@@ -10,7 +9,7 @@ import { Contact } from '../../components/contact/contact';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header, Footer, About, Education, Publications, Contact, Social],
+  imports: [RouterOutlet, Header, Footer, About, Education, Publications, Contact],
   templateUrl: './layout.html',
 })
 export class Layout {
